f3205302097034db0d644d2e85f5102f909d65ec
suer <sue@nagoya-u.jp>
2011-10-20 02:48:10 +0900

    edit file

--- app/controllers/codes.rb
+++ app/controllers/codes.rb
@@ -10,18 +10,35 @@ Debeso.controllers :codes do
   end
 
   post :create_repository do
-    dir = Setting[:repository_root] + "/" + Digest::SHA1.hexdigest(Time.now.to_s)
+    id = Digest::SHA1.hexdigest(Time.now.to_s)
+    dir = Setting[:repository_root] + "/" + id
     Dir::mkdir(dir)
     git = Git.init(dir)
     file = dir + "/file.txt"
     open(file, "w") {}
     git.add(file)
     git.commit("init")
-    redirect url(:codes, :edit, :id => dir)
+    redirect url(:codes, :edit, :id => id)
   end
 
-  get :edit do
-    render 'codes/edit'
+  get :edit, :with => :id do
+    @id = params[:id]
+    file = Setting[:repository_root] + "/" + @id + "/file.txt"
+    open(file) {|f| @content = f.read}
+    render "codes/edit"
+  end
+
+  post :edit, :with => :id do
+    @id = params[:id]
+    @content = params[:content]
+    dir = Setting[:repository_root] + "/" + @id
+    file = dir + "/file.txt"
+    
+    puts file
+    open(file, "w") {|f| puts @content;f.write(@content)}
+    git = Git.open(dir)
+    git.commit_all("update")
+    redirect url(:codes, :edit, :id => @id)
   end
 
 end
