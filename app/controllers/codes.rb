9c5eff02bcaa1ba4f7bcca6b75437226dd239501
suer <sue@nagoya-u.jp>
2011-10-20 02:05:22 +0900

    init and add file

--- app/controllers/codes.rb
+++ app/controllers/codes.rb
@@ -1,4 +1,5 @@
 require 'digest/sha1'
+require 'git'
 Debeso.controllers :codes do
 
   get :index, :map => '/' do
@@ -9,8 +10,18 @@ Debeso.controllers :codes do
   end
 
   post :create_repository do
-    Dir::mkdir(Setting[:repository_root] + "/" + Digest::SHA1.hexdigest(Time.now.to_s))
-    redirect url(:codes, :index)
+    dir = Setting[:repository_root] + "/" + Digest::SHA1.hexdigest(Time.now.to_s)
+    Dir::mkdir(dir)
+    git = Git.init(dir)
+    file = dir + "/file.txt"
+    open(file, "w") {}
+    git.add(file)
+    git.commit("init")
+    redirect url(:codes, :edit, :id => dir)
+  end
+
+  get :edit do
+    render 'codes/edit'
   end
 
 end
