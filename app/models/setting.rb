65e9d400cea768de65880df90f971e79e62ac7b1
mallowlabs <mallowlabs@gmail.com>
2011-10-30 20:30:10 +0900

    default setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,8 @@
 class Setting
-  SETTING_PATH = File.dirname(__FILE__) + "/../../config/setting.yml"
-  @@available_settings = YAML.load(File.open(SETTING_PATH))
+  SETTING_PATH = File.dirname(__FILE__) + '/../../config/setting.yml'
+  @@available_settings = File.exist?(SETTING_PATH) ?
+                         YAML.load(File.open(SETTING_PATH)) :
+                         { 'repository_root' => File.dirname(__FILE__) + "/../../tmp/debeso"}
   def self.[](key)
     @@available_settings[key.to_s]
   end
