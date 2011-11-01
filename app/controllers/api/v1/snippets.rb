82d45ab151e3592922806ade9d3a16834c2ddfda
suer <sue@nagoya-u.jp>
2011-11-01 22:41:16 +0900

    iframe

--- app/controllers/api/v1/snippets.rb
+++ app/controllers/api/v1/snippets.rb
@@ -5,18 +5,25 @@ Debeso.controllers :"api/v1/snippets" do
   end
 
   get :index, :with => :id, :provides => [:json, :html] do
-    id = params[:id]
-    @snippet = Snippet.where(:sha1_hash => id).first
-    open("#{@repository_root}/#{id}.txt") {|f| @snippet.content = f.read}
+    @id = params[:id]
+    @snippet = Snippet.where(:sha1_hash => @id).first
+    open("#{@repository_root}/#{@id}.txt") {|f| @snippet.content = f.read}
     if params[:format] == 'html'
       render 'api/v1/index', :layout => false
     else params[:format] == 'json'
       unless @snippet.nil?
         @snippet.to_hash.to_json
       else
-        {:message => "snippet #{id} not found"}.to_json
+        {:message => "snippet #{@id} not found"}.to_json
       end
     end
+  end
+
+  get :show, :with => :id, :provides => :html do
+    @id = params[:id]
+    @snippet = Snippet.find_by_hash(@id)
+    open("#{@repository_root}/#{@id}.txt") {|f| @snippet.content = f.read}
+    render 'api/v1/show', :layout => false
   end
 
 end
