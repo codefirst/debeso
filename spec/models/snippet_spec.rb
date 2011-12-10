a80f2574cb783a0bc5a3c43cf64e08899920f18a
suer <sue@nagoya-u.jp>
2011-12-11 03:22:46 +0900

    test refs #872

--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -8,6 +8,12 @@ describe "Snippet Model" do
       s.created_at = '2011/12/31'
       s.updated_at = '2012/1/15'
     end
+    Git = mock()
+    git = mock()
+    Git.stub(:open) { git }
+    blob = mock()
+    git.stub(:object) { blob }
+    blob.stub(:contents) { "print 'hoge'" }
   end
   it "to hash" do
     @snippet.to_hash[:id].should == 1
